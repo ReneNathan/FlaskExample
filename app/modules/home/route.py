57fed1009a58b7675f64e27f4668026da3e1e4e3
ReneNathan <rene.nathan@cajamar.sp.gov.br>
2025-05-14 13:54:57 -0300

    :sparkles: Melhoria na esturtura de pastas, criação do reset.css e demais arquivos emm branco para uso futuro

--- app/modules/home/route.py
+++ app/modules/home/route.py
@@ -1,6 +1,13 @@
 from flask import Blueprint, render_template
 
-home_bp = Blueprint("home", __name__, template_folder="templates", url_prefix="/")
+home_bp = Blueprint(
+    "home",
+    __name__,
+    template_folder="templates",
+    static_folder="static",  # <--- precisa disso
+    static_url_path="/home/static",  # <--- define a URL para os arquivos estáticos
+    url_prefix="/",
+)
 
 
 @home_bp.route("/")
